e3578ccc22e4a2680f7331cf2d3ad50ddbbb0211
Gmmolly05 <gmm9098@rit.edu>
2026-07-09 22:06:21 -0400

    Removed unnecessary handle comment

--- main.js
+++ main.js
@@ -13,7 +13,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 ipcMain.handle('get-tasks', () => loadTasks(dataPath));
-//ipcMain.handle('get-lists', () => loadTasks(dataPath));
 ipcMain.handle('set-tasks', (event, tasks) => saveTasks(dataPath, tasks));
 ipcMain.handle('minimize-window', () => BrowserWindow.getFocusedWindow().minimize());
 ipcMain.handle('close-window', () => BrowserWindow.getFocusedWindow().close());
